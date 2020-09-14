a72304ea10152beb3964bcebdd3736ecc2f729ef
dstroj <dstroj@wp.pl>
2020-09-14 18:19:54 +0200

    initial commit

---
+++ src/main/java/pl/strojecki/springpoints/SpringpointsApplication.java
@@ -0,0 +1,13 @@
+package pl.strojecki.springpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/strojecki/springpoints/SpringpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.strojecki.springpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
